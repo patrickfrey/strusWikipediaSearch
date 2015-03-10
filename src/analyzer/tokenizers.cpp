1ad06333d1e16e6c00cf232baeab5e2faae09f40
patrickfrey <patrickpfrey@yahoo.com>
2015-03-10 21:45:12 +0100

    bugfixes

--- src/analyzer/tokenizers.cpp
+++ src/analyzer/tokenizers.cpp
@@ -32,6 +32,7 @@
 #include <cstring>
 #include <vector>
 #include <stdexcept>
+/*[-]*/#include <iostream>
 
 using namespace strus;
 
@@ -49,9 +50,56 @@ static const char* findPattern( const std::string& pattern, const char* si, cons
 	return 0;
 }
 
+static bool isAlphaNum( char ch)
+{
+	return ((ch|32) >= 'a' && (ch|32) <= 'z') || (ch >= '0' && ch <= '9');
+}
+
+static bool isSpace( char ch)
+{
+	return (ch <= 32);
+}
+
+const char* skipIdentifier( char const* si, const char* se)
+{
+	for (; si != se && isAlphaNum(*si); ++si){}
+	return si;
+}
+
+const char* skipSpace( char const* si, const char* se)
+{
+	for (; si != se && isSpace(*si); ++si){}
+	return si;
+}
+
+const char* skipValue( char const* si, const char* se)
+{
+	if (si != se)
+	{
+		if (*si == '&')
+		{
+			if (0==std::memcmp( si, "&quot;", 6))
+			{
+				si += 6;
+				const char* ni = findPattern( "&quot;", si, se);
+				return ni;
+			}
+			else
+			{
+				return si + 1;
+			}
+		}
+		else if (isAlphaNum(*si))
+		{
+			return skipIdentifier( si, se);
+		}
+	}
+	return si;
+}
+
 const char* skipUrl( char const* si, const char* se, char delim)
 {
-	for (; si != se && (*si|32) >= 'a' && (*si|32) <= 'z'; ++si){}
+	for (; si != se && isAlphaNum(*si); ++si){}
 	if (si[0] == ':' && si[1] == '/')
 	{
 		return (const char*)std::memchr( si, delim, se-si);
@@ -65,41 +113,110 @@ static const char* skipToToken( char const* si, const char* se)
 	{
 		if (*si == '&')
 		{
-			if (std::memcmp( si, "&quot;", 6) == 0)
+			if (0==std::memcmp( si, "&quot;", 6))
 			{
 				si += 6;
-				continue;
 			}
-			if (std::memcmp( si, "&lt;", 6) == 0)
+			else if (0==std::memcmp( si, "&amp;", 5))
 			{
-				si += 4;
-				si = findPattern( "&gt;", si, se);
-				continue;
+				si += 5;
+			}
+			else if (0==std::memcmp( si, "&lt;", 4))
+			{
+				if (0==std::memcmp( si, "&lt;!--;", 7))
+				{
+					si += 7;
+					si = findPattern( "--&gt;", si, se);
+				}
+				else if (0==std::memcmp( si, "&lt;math&gt;", 12))
+				{
+					si += 12;
+					si = findPattern( "&lt;/math&gt;", si, se);
+				}
+				else if (0==std::memcmp( si, "&lt;span&gt;", 12))
+				{
+					si += 12;
+					si = findPattern( "&lt;/span&gt;", si, se);
+				}
+				else if (0==std::memcmp( si, "&lt;pre&gt;", 11))
+				{
+					si += 11;
+					si = findPattern( "&lt;/pre&gt;", si, se);
+				}
+				else if (0==std::memcmp( si, "&lt;div&gt;", 11))
+				{
+					si += 11;
+					si = findPattern( "&lt;/div&gt;", si, se);
+				}
+				else if (0==std::memcmp( si, "&lt;gallery&gt;", 15))
+				{
+					si += 15;
+					si = findPattern( "&lt;/gallery&gt;", si, se);
+				}
+				else if (0==std::memcmp( si, "&lt;nowiki&gt;", 14))
+				{
+					si += 14;
+					si = findPattern( "&lt;/nowiki&gt;", si, se);
+				}
+				else
+				{
+					si += 4;
+					si = findPattern( "&gt;", si, se);
+				}
+			}
+			else
+			{
+				++si;
 			}
 		}
 		else if (*si == '[')
 		{
 			++si;
-			if (si < se && si[1] == '[')
+			if (si < se && *si == '[')
 			{
-				si = findPattern( "]]", si, se);
-				continue;
+				si = findPattern( "]]", si+1, se);
 			}
 			char const* ue = skipUrl( si, se, ']');
 			if (ue)
 			{
 				si = ue+1;
-				continue;
 			}
 		}
 		else if (*si == '{')
 		{
 			++si;
-			if (si < se && si[1] == '{')
+			if (si < se && *si == '{')
 			{
-				si = findPattern( "}}", si, se);
-				continue;
+				si = findPattern( "}}", si+1, se);
 			}
+			else if (si < se && *si == '|')
+			{
+				const char* xi = (const char*)std::memchr( si, '\n', se-si);
+				if (xi) si=xi+1;
+			}
+			else
+			{
+				const char* xi = (const char*)std::memchr( si, '}', se-si);
+				if (xi) si=xi+1;
+			}
+		}
+		else if (*si == '!' || *si == '|')
+		{
+			++si;
+			char const* ai = skipIdentifier( skipSpace( si, se), se);
+			if (ai < se && *ai == '=')
+			{
+				si = skipValue( ai+1, se);
+			}
+			si = skipSpace( si, se);
+		}
+		else if (*si <= 32)
+		{
+			si = skipSpace( si, se);
+		}
+		else
+		{
+			break;
 		}
 	}
 	return (si)?si:se;
@@ -125,14 +242,14 @@ private:
 
 static void tokenizeWordSeparation( std::vector<analyzer::Token>& res, const char* src, char const* si, const char* se)
 {
-	static const CharTable delimiter("{}[]<>&:.;,!?%/()+-'\"`=");
+	static const CharTable delimiter("#|{}[]<>&:.;,!?%/()+-'\"`=");
 	for (si = skipToToken( si, se); si != se; si = skipToToken( si, se))
 	{
-		while (si != se && ((unsigned char)*si <= 32 || delimiter[ *si])){}
+		for (;si != se && ((unsigned char)*si <= 32 || delimiter[ *si]); ++si){}
 		if (si != se)
 		{
 			char const* start = si;
-			while (si != se && ((unsigned char)*si > 32 && !delimiter[ *si])){}
+			for (;si != se && ((unsigned char)*si > 32 && !delimiter[ *si]); ++si){}
 			res.push_back( analyzer::Token( start - src, si - start));
 		}
 	}
@@ -140,15 +257,15 @@ static void tokenizeWordSeparation( std::vector<analyzer::Token>& res, const cha
 
 static void tokenizeWhiteSpace( std::vector<analyzer::Token>& res, const char* src, char const* si, const char* se)
 {
-	static const CharTable delimiter("{[&");
-	
+	static const CharTable delimiter("'{}[]&");
+
 	for (si = skipToToken( si, se); si != se; si = skipToToken( si, se))
 	{
-		while (si != se && ((unsigned char)*si <= 32 || delimiter[ *si])){}
+		for (;si != se && ((unsigned char)*si <= 32 || delimiter[ *si]); ++si){}
 		if (si != se)
 		{
 			char const* start = si;
-			while (si != se && ((unsigned char)*si > 32 && !delimiter[ *si])){}
+			for (; si != se && ((unsigned char)*si > 32 && !delimiter[ *si]); ++si){}
 			res.push_back( analyzer::Token( start - src, si - start));
 		}
 	}
@@ -239,6 +356,11 @@ public:
 			{
 				if (arg.size() > 1) throw std::runtime_error( "too many arguments for 'link' tokenizer");
 				m_name = arg[0];
+				m_pattern = getPattern( m_name);
+			}
+			else
+			{
+				m_name = "";
 				m_pattern = getPattern( m_name);
 			}
 		}
@@ -335,7 +457,14 @@ public:
 						case Id:
 						{
 							start = (const char*)std::memchr( si, '|', pe-si);
-							if (!start) start = si;
+							if (start)
+							{
+								++start;
+							}
+							else
+							{
+								start = si;
+							}
 							tokenizeTokenClass( m_tokenClass, rt, src, start, pe);
 						}
 					}
