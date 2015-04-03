23ff1e4fc9fb53377ee6660f4b935f745cbdbb25
patrickfrey <patrickpfrey@yahoo.com>
2015-04-03 14:59:46 +0200

    temporary checkin

--- php/collectionStats.php
+++ php/collectionStats.php
@@ -3,8 +3,19 @@
 <body>
 
 <?php
-echo "My first PHP script!";
+require "strus.php";
+
+try {
+	$context = new StrusContext();
+	$storage = $context->createStorageClient( "path=/home/patrick/Projects/github/strusWikipediaSearch/storage");
+
+	echo "Number of documents inserted: " . $storage->nofDocumentsInserted() . "!";
+}
+catch ( Exception $e) {
+	echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
 ?>
 
 </body>
 </html> 
+
