e173141da4984c6e8555c450e2e063697bea2473
patrickfrey <patrickpfrey@yahoo.com>
2015-05-13 14:41:24 +0200

    ...

--- scripts/euroserver/startup_server1.sh
+++ scripts/euroserver/startup_server1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-strusRpcServer -m analyzer_wikipedia_search -s "path=/data/wikipedia/storage1" -R resources/ -p 7181 -g storage_stats1.txt
+strusRpcServer -m analyzer_wikipedia_search -s "path=/data/wikipedia/storage1" -R resources/ -p 7181 -g resources/storage_stats1.txt
 
