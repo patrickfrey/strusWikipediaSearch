028b1de033f094d3965fa02e52402e0eaf46d485
patrickfrey <patrickpfrey@yahoo.com>
2015-05-13 17:38:07 +0200

    remove height=100% from strus logo img

--- client/evalQuery.php
+++ client/evalQuery.php
@@ -10,7 +10,7 @@
 <div id="search_form">
 <div id="search_logo">
  <a target="_blank" href="http://project-strus.net">
-  <img style="display:block;" width="100%" height="100%" src="strus_logo.jpg" alt="strus logo">
+  <img style="display:block;" width="100%" src="strus_logo.jpg" alt="strus logo">
 <!-- Copyright: <a href='http://www.123rf.com/profile_guarding123'>guarding123 / 123RF Stock Photo</a>
 -->
  </a>
