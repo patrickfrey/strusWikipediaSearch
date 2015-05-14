a622dd1a305badb644d0f4f4e29174c3009785a3
patrickfrey <patrickpfrey@yahoo.com>
2015-05-14 11:19:38 +0200

    ...

--- scripts/euroserver/startup_server2.sh
+++ scripts/euroserver/startup_server2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-strusRpcServer -m analyzer_wikipedia_search -s "path=/data/wikipedia/storage2" -R resources/ -p 7182 -g resources/storage_stats2.txt
+strusRpcServer -m analyzer_wikipedia_search -s "path=/data/wikipedia/storage2" -R /home/pfrey/github/strusWikipediaSearch/resources/ -p 7182 -g /home/pfrey/github/strusWikipediaSearch/resources/storage_stats2.txt
