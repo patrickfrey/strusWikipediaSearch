355cba6879948a7dfe2e87b7c35925a6bec8e884
patrickfrey <patrickpfrey@yahoo.com>
2015-05-22 13:43:02 +0200

    changed startup script for euroserver (on storage only)

---
+++ scripts/euroserver/startup_server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+strusRpcServer -m analyzer_wikipedia_search -s "path=/data/wikipedia/storage" -R /home/pfrey/github/strusWikipediaSearch/resources/ -p 7181
+
--- scripts/euroserver/startup_server2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-strusRpcServer -m analyzer_wikipedia_search -s "path=/data/wikipedia/storage2" -R /home/pfrey/github/strusWikipediaSearch/resources/ -p 7182 -g /home/pfrey/github/strusWikipediaSearch/resources/storage_stats2.txt
