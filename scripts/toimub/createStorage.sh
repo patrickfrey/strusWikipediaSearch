bd88052bfe7870b22df04255757f076eafa4417b
patrickfrey <patrickpfrey@yahoo.com>
2015-05-22 13:54:23 +0200

    some insert scripts changed

---
+++ scripts/toimub/createStorage.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+strusCreate "path=storage; metadata=doclen_tist UInt8,doclen UInt16,pageweight Float32,contribid UInt32"
--- scripts/toimub/partialInsert2.sh
+++ scripts/toimub/partialInsert2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bzip2 -d -c /data/*.xml.bz2 | strusWikimediaToXml -s -r30G,100G - | strusInsert -c500 -f1 -n -m analyzer_wikipedia_search -R /home/pfrey/github/strusWikipediaSearch/resources/ "path=/data/storage2" /home/pfrey/github/strusWikipediaSearch/config/wikipedia.ana -
+bzip2 -d -c /data/*.xml.bz2 | strusWikimediaToXml -s -r30G,100G - | strusInsert -c500 -f1 -n -m analyzer_wikipedia_search -R /home/pfrey/github/strusWikipediaSearch/resources/ -s "path=/data/storage2" /home/pfrey/github/strusWikipediaSearch/config/wikipedia.ana -
 echo "30G,..rest" inserted into storage 2
