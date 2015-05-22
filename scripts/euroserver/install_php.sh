175a32e55fa448b00851b700596494f5e8f23559
PatrickFrey <patrickpfrey@yahoo.com>
2015-05-22 16:53:11 +0200

    ...

--- scripts/euroserver/install_php.sh
+++ scripts/euroserver/install_php.sh
@@ -3,4 +3,3 @@
 cp client/*.php /srv/http/patrickfrey.dyndns.org/
 cp client/*.css /srv/http/patrickfrey.dyndns.org/
 cp client/*.html /srv/http/patrickfrey.dyndns.org/
-cp client/evalQuery_euroserver.php /srv/http/patrickfrey.dyndns.org/evalQuery.php
