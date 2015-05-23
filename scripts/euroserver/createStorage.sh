31eb335eb113e3d1e26cd21552b380d8a62c3c64
PatrickFrey <patrickpfrey@yahoo.com>
2015-05-23 18:55:41 +0200

    some euroserver insert script changes

---
+++ scripts/euroserver/createStorage.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+strusCreate "path=/data/wikipedia/storage; metadata=doclen_tist UInt8,doclen UInt16,pageweight Float32,contribid UInt32"
+
---
+++ scripts/euroserver/insertDocuments.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+time bzip2 -c -d /data/*.xml.bz2 | strusWikimediaToXml -s -r0G,30G - | strusInsert -c2000 -f1 -n -m analyzer_wikipedia_search -R resources/ -s "path=/data/wikipedia/storage" config/wikipedia.ana -
+echo "0G,30G" inserted into storage 
