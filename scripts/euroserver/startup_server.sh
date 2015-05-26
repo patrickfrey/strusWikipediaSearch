c2a07921d78f83161364bcfcdd0666471040073f
patrickfrey <patrickpfrey@yahoo.com>
2015-05-26 14:24:39 +0200

    adapted euroserver scripts (new storage built)

--- scripts/euroserver/startup_server.sh
+++ scripts/euroserver/startup_server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-strusRpcServer -m analyzer_wikipedia_search -s "path=/data/wikipedia/storage" -R /home/pfrey/github/strusWikipediaSearch/resources/ -p 7181
+strusRpcServer -m analyzer_wikipedia_search -s "path=/data/storage_xs03" -R /home/pfrey/github/strusWikipediaSearch/resources/ -p 7181
 
