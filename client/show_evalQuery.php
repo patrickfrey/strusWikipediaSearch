012ca597792979abff3d5a91ecdae561946029b7
patrickfrey <patrickpfrey@yahoo.com>
2015-05-27 16:50:39 +0200

    added link to PHP client source code

---
+++ client/show_evalQuery.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP client source of demo search on Wikipedia with strus</title>
+<link href="strus.css" rel="stylesheet" type="text/css">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<h1>PHP client source of demo search on Wikipedia (english) with strus</h1>
+<pre>
+<?php
+	echo htmlspecialchars( file_get_contents( "evalQuery.php" ))
+?>
+
+</body>
+</html>
