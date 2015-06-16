13a7631d14ebcdf7868fad7a5cf6a1c1b41bde35
patrickfrey <patrickpfrey@yahoo.com>
2015-06-16 13:49:39 +0200

    adapted module interface changes

--- src/analyzer/modstrus_analyzer_wikipedia_search.cpp
+++ src/analyzer/modstrus_analyzer_wikipedia_search.cpp
@@ -307,7 +307,7 @@ static const strus::NormalizerConstructor normalizers[] =
 
 extern "C" DLL_PUBLIC strus::AnalyzerModule entryPoint;
 
-strus::AnalyzerModule entryPoint( tokenizers, normalizers);
+strus::AnalyzerModule entryPoint( tokenizers, normalizers, 0);
 
 
 
