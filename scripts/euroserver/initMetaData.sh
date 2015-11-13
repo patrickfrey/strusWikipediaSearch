c277c1e03afd89ed30097b19df35074fac20fdce
patrickfrey <patrickpfrey@yahoo.com>
2015-11-13 09:48:23 +0100

    adapted euroserver storage patch script

--- scripts/euroserver/initMetaData.sh
+++ scripts/euroserver/initMetaData.sh
@@ -7,21 +7,23 @@
 # pageweight Float32
 #
 
+STORAGEPATH="/data/storage_xs09"
+
 echo "Initializing the document title length attribute ..."
-strusInspect -s "path=/data/storage_xs03" ttc tist >  resources/metadata_tist_doclen.txt
-strusUpdateStorage -s "path=/data/storage_xs03" -m doclen_tist resources/metadata_tist_doclen.txt
+strusInspect -s "path=$STORAGEPATH" ttc tist >  resources/metadata_tist_doclen.txt
+strusUpdateStorage -s "path=$STORAGEPATH" -m doclen_tist resources/metadata_tist_doclen.txt
 echo "... done"
 
 echo "Initializing the link popularity weight in meta data ..."
-strusInspect -s "path=/data/storage_xs03" fwstats linkid > resources/linkid_list.txt
+strusInspect -s "path=$STORAGEPATH" fwstats linkid > resources/linkid_list.txt
 echo "... get the document id's"
-strusInspect -s "path=/data/storage_xs03" attribute title > resources/docid_list.txt
+strusInspect -s "path=$STORAGEPATH" attribute title > resources/docid_list.txt
 echo "... calculate a map docno -> number of references to this page"
 scripts/calcDocidRefs.pl resources/docid_list.txt resources/linkid_list.txt > resources/docnoref_map.txt
 echo "... calculate a map docno -> link popularity weight"
 scripts/calcWeights.pl resources/docnoref_map.txt 'tanh(x/50)' > resources/pageweight_map.txt
 echo "... update the meta data table element pageweight with the link popularity weight"
-strusUpdateStorage -s "path=/data/storage_xs03" -m pageweight resources/pageweight_map.txt
+strusUpdateStorage -s "path=$STORAGEPATH" -m pageweight resources/pageweight_map.txt
 echo "... done"
 
 
