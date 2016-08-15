e28a728df92d7fab63f5094dec87834e409d3393
patrickfrey <patrickpfrey@yahoo.com>
2016-08-15 12:33:02 +0200

    index new set of articles 8/2016

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget -q -O - http://dumps.wikimedia.your.org/enwiki/20160204/enwiki-20160204-pages-articles.xml.bz2 | bzip2 -d -c | strusWikimediaToXml -f "data/wikipedia%04u.xml,20M" -n0 -s -
+wget -q -O - http://dumps.wikimedia.your.org/enwiki/20160801/enwiki-20160801-pages-articles.xml.bz2 | bzip2 -d -c | strusWikimediaToXml -f "data/wikipedia%04u.xml,20M" -n0 -s -
 
