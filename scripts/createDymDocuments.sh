34272d8b06fb3cf9912420188a0beebe3cbf1fda
patrickfrey <patrickpfrey@yahoo.com>
2016-08-17 19:13:41 +0200

    ...

--- scripts/createDymDocuments.sh
+++ scripts/createDymDocuments.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm data/dymitems.txt
-for dd in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 ; do echo "-------- $dd"; scripts/getDymSegments.sh data/wikipedia$dd.tar.gz; done
+for dd in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 ; do echo "-------- $dd"; scripts/getDymSegments.sh data/wikipedia$dd.tar.gz; done
 sort data/dymitems.txt | uniq > data/dymitems_sorted.txt
 mv data/dymitems_sorted.txt data/dymitems.txt
 
