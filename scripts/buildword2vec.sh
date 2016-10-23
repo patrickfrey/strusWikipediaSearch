207537bb6da09972805119d93a32e8fb17e458b5
patrickfrey <patrickpfrey@yahoo.com>
2016-10-23 11:13:30 +0200

    fixed sed for unicode dash extraction for NLP

--- scripts/buildword2vec.sh
+++ scripts/buildword2vec.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-outprefix = nlpdata/
+outprefix=nlpdata/
 
 runNLP() {
 	jobid=$1
 	infiles=$2
-	dmp_outputfile = "$outprefix""docs.dump.$jobid.txt"
-	nlp_outputfile = "$outprefix""docs.nlp.$jobid.txt"
-	dic_outputfile = "$outprefix""dict.$jobid.txt"
+	dmp_outputfile="$outprefix""docs.dump.$jobid.txt"
+	nlp_outputfile="$outprefix""docs.nlp.$jobid.txt"
+	dic_outputfile="$outprefix""dict.$jobid.txt"
 	rm $dmp_outputfile
 	rm $nlp_outputfile
 	for dd in $infiles ; do echo "-------- $dd"; github/strusWikipediaSearch/scripts/nlpdump.sh data/wikipedia$dd.tar.gz $jobid "$outprefix"tmp $dmp_outputfile; done
@@ -16,17 +16,21 @@ runNLP() {
 	github/strusWikipediaSearch/scripts/strusnlp.py dict $nlp_outputfile 3 > $dic_outputfile
 }
 
-runNLP 1 "00 13 03 16" &
-runNLP 2 "06 19 09 22" &
-runNLP 3 "01 14 04 17" &
-runNLP 4 "07 20 10 23 25" &
-runNLP 5 "02 15 05 18 12" &
-runNLP 6 "08 21 11 24 26" &
+runNLP 1 "00 13 08" &
+runNLP 2 "03 16 21" &
+runNLP 3 "06 19 11" &
+runNLP 4 "09 22 24" &
+runNLP 5 "01 14 26" &
+runNLP 6 "04 17" &
+runNLP 7 "07 20" &
+runNLP 8 "10 23 25" &
+runNLP 9 "02 15" &
+runNLP 0 "05 18 12" &
 
-scripts/strusnlp.py joindict dict.{1,2,3,4,5,6}.txt > "$outprefix"dict.txt
-rm dict.{1,2,3,4,5,6}.txt
+scripts/strusnlp.py joindict "$outprefix"dict.{0,1,2,3,4,5,6,7,8,9}.txt > "$outprefix"dict.txt
+rm dict.{0,1,2,3,4,5,6,7,8,9}.txt
 
-for dd in 1 2 3 4 5 6
+for dd in 0 1 2 3 4 5 6 7 8 9
 do
 scripts/strusnlp.py concat "$outprefix"docs.nlp.$jobid.txt "$outprefix"dict.txt > "$outprefix"docs.word2vec.$dd.txt
 done
--- scripts/nlpdump.sh
+++ scripts/nlpdump.sh
@@ -18,7 +18,7 @@ tar -C $prefix$jobid/ -xvzf $1
 for ff in `find $prefix$jobid/ -name "*.xml"`
 do
 echo "analyze $ff"
-strusAnalyze -M /usr/local/lib/strus/modules -m analyzer_wikipedia_search -R resources/ -D "orig,sent=' .\n',para=' .\n',start=' .\n'" config/wikipedia.ana $ff | iconv -c -f utf-8 -t utf-8 - | perl -C -pe 's/\&[a-z]*\;/ /g' | perl -C -pe 's/nbsp[\;]/ /g' | perl -C -pe 's/[—––_=\/;,()\[\]\{\}\"-\-]/ /g' | perl -C -pe 's/([\!\?\:\.])/ \1 /g' | perl -C -pe 's/[ ]+/ /g' | perl -C -pe 's/[ ][.][ ]/ .\n/g'  >> $outputfile
+strusAnalyze -M /usr/local/lib/strus/modules -m analyzer_wikipedia_search -R resources/ -D "orig,sent=' .\n',para=' .\n',start=' .\n'" config/wikipedia.ana $ff | iconv -c -f utf-8 -t utf-8 - | perl -C -pe 's/\&[a-z]*\;/ /g' | perl -C -pe 's/nbsp[\;]/ /g' | perl -C -pe 's/[_=\/;,()\[\]\{\}\"-\-]/ /g' | sed 's/\xe2\x80\x94/ /g' | sed 's/\xe2\x80\x93/ /g' | perl -C -pe 's/([\!\?\:\.])/ \1 /g' | perl -C -pe 's/[ ]+/ /g' | perl -C -pe 's/[ ][.][ ]/ .\n/g'  >> $outputfile
 done
 
 rm -Rf $prefix$jobid/
