d9defb01cbdfd37120531606d9f387cf5419118d
patrickfrey <patrickpfrey@yahoo.com>
2016-10-30 13:21:53 +0100

    improved entity split algorithm

--- scripts/strusnlp.py
+++ scripts/strusnlp.py
@@ -41,30 +41,32 @@ def fill_nnp_split_dict():
 
 def nnp_left_weight( word):
     rt = 0.0
+    norm = 1.0
     if word in nnp_left_dict:
-        rt = math.log(nnp_dict[ word]) / (1.0 + math.log(nnp_left_dict[ word]))
-#        print "    LEFT OCCUR '%s' %f %f" % (word, float(nnp_dict[ word]), float(nnp_left_dict[ word]))
-    else:
-        rt = math.log(nnp_dict[ word])
-#        print "    LEFT OCCUR '%s' %f" % (word, float(nnp_dict[ word]))
+        norm += math.log( nnp_left_dict[ word])
+    if word in nnp_dict:
+        rt = math.log( nnp_dict[ word]) / norm
     if len(word) > 4 and word[ -1] == 's':
+        rt2 = rt
+        norm2 = 1.0
         if word[:-1] in nnp_left_dict:
-            rt += math.log(nnp_dict[ word[:-1]]) / (1.0 + math.log(nnp_left_dict[ word[:-1]]))
-        else:
-            rt += math.log(nnp_dict[ word[:-1]])
-        rt /= 2
-    return rt
+            norm2 += math.log( nnp_left_dict[ word[:-1]])
+        if word[:-1] in nnp_dict:
+            rt2 = math.log(nnp_dict[ word[:-1]]) / norm2
+        rt = (rt + rt2) / 2
+        if norm2 > norm:
+            norm = norm2
+    return rt,norm
 
 def nnp_right_weight( word):
+    norm = 1.0
     rt = 0.0
+    if word in nnp_right_dict:
+        norm += math.log( nnp_right_dict[ word])
     if word in nnp_dict:
-        if word in nnp_right_dict:
-            rt = math.log( nnp_dict[ word]) / (1.0 + math.log(nnp_right_dict[ word]))
-#            print "    RIGHT OCCUR '%s' %f %f" % (word, float(nnp_dict[ word]), float(nnp_right_dict[ word]))
-        else:
-            rt = math.log(nnp_dict[ word])
-#            print "    RIGHT OCCUR '%s' %f" % (word, float(nnp_dict[ word]))
-    return rt
+        rt = math.log( nnp_dict[ word]) / norm
+    return rt,norm
+
 
 def nnp_split( seqword):
 #    print "SPLIT '%s'" % seqword
@@ -74,22 +76,28 @@ def nnp_split( seqword):
         seqlen += 1
         halfsize = seqword.find('_',halfsize+1)
     candidates = []
-    if seqword in nnp_dict:
-        candidates.append( [ None, math.log( nnp_dict[ seqword]) ] )
+    first_norm = 1.0
     halfsize = seqword.find('_')
     len1 = 0
     len2 = seqlen
     while halfsize != -1:
         half1 = seqword[ 0:halfsize]
-        w1 = nnp_left_weight( half1)
+        w1,n1 = nnp_left_weight( half1)
         len1 += 1
         half2 = seqword[ (halfsize+1):]
-        w2 = nnp_right_weight( half2)
+        w2,n2 = nnp_right_weight( half2)
         len2 -= 1
+        if first_norm < n1:
+            first_norm = n1
+        if first_norm < n1:
+            first_norm = n2
 #        print "    WEIGHT '%s' %f" % (half1, w1)
 #        print "    WEIGHT '%s' %f" % (half2, w2)
         candidates.append( [halfsize, w1 + w2] )
         halfsize = seqword.find('_',halfsize+1)
+    if seqword in nnp_dict:
+#        print "    FIRST WEIGHT '%s' %f (%u %f)" % (seqword, math.log( nnp_dict[ seqword]) / first_norm, nnp_dict[ seqword], first_norm)
+        candidates.append( [ None, math.log( nnp_dict[ seqword]) / first_norm ] )
     best_halfsize = None
     best_weight = 0.0
     for cd in candidates:
@@ -371,9 +379,9 @@ elif cmd == "splitdict" or cmd == "splittest":
         for key,value in new_dict.iteritems():
             print "%s %u" % (key,value)
     else: #cmd == "splittest"
-        key = sys.argv[3]
-        for word in nnp_split_words( key):
-            print "%s" % word
+        for key in sys.argv[3:]:
+            for word in nnp_split_words( key):
+                print "%s" % word
 
 elif cmd == "seldict":
     dictfile = sys.argv[2]
