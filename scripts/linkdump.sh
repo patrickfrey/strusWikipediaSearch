950fc336cee327dec44a82855473d47490d7e977
patrickfrey <patrickpfrey@yahoo.com>
2017-01-12 22:15:31 +0100

    added script to dump links to a file for further processing

---
+++ scripts/linkdump.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+tarfile=$1
+jobid=$2
+prefix=$3
+srcprefix=$4
+
+if [ x$prefix = 'x' ]
+then
+	prefix=tmp
+fi
+
+mkdir -p $prefix$jobid
+tar -C $prefix$jobid/ -xvzf $1
+for ff in `find $prefix$jobid/ -name "*.xml" | sort`
+do
+strusAnalyze -M /usr/local/lib/strus/modules -m analyzer_wikipedia_search -R "$srcprefix"resources/ -D "docid,linkid,start='\n-\n',linkvar='\n\t'" "$srcprefix"config/wikipedia.ana $ff | iconv -c -f utf-8 -t utf-8 - | "$srcprefix"scripts/nlpclean.sh | perl -C -pe 's/[~_=\/\#-\+\-]/ /g' | perl -C -pe 's/[\<\>()\|\[\]\{\}]/ \,/g' | perl -C -pe 's/[,]+\./\./g' | perl -C -pe 's/([\.])(^[0-9])/ \1 \2/g' | perl -C -pe 's/([\;\,\!\?\:])/ \1 /g' | perl -C -pe 's/([\\])/ /g'
+done
+
+rm -Rf $prefix$jobid/
+
