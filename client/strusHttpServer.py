7c73eb9f907cfb508e4e3ca88250ff64e7617b3d
patrickfrey <patrickpfrey@yahoo.com>
2017-02-02 11:56:27 +0100

    ...

--- client/strusHttpServer.py
+++ client/strusHttpServer.py
@@ -440,12 +440,12 @@ class QueryHandler( tornado.web.RequestHandler ):
             relatedterms = None
 
             if scheme == "NBLNK" or scheme == "TILNK" or scheme == "VCLNK" or scheme == "STDLNK":
-                selectresult = yield self.evaluateQuery( scheme, querystruct, 0, 100, restrictdn)
+                selectresult = yield self.evaluateQuery( scheme, querystruct, 0, 120, restrictdn)
                 errors += selectresult[1]
-                result = [self.getLinkQueryResults( selectresult[0], 'links', firstrank, nofranks), errors]
+                result = [self.getLinkQueryResults( selectresult[0], 'links', firstrank, nofranks+1), errors]
             elif scheme == "STD":
                 noflinks = 20
-                selectresult = yield self.evaluateQuery( "STDLNK", querystruct, 0, 100, 0)
+                selectresult = yield self.evaluateQuery( "STDLNK", querystruct, 0, 120, 0)
                 errors += selectresult[1]
                 links = self.getLinkQueryResults( selectresult[0], 'links', 0, noflinks)
                 if len(links) >= 1:
