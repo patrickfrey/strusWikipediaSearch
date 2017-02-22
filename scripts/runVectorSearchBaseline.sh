02137ab71e9ee0e040d65a199be47208b482e7a9
patrickfrey <patrickpfrey@yahoo.com>
2017-02-22 19:46:54 +0100

    ...

--- scripts/runVectorSearchBaseline.sh
+++ scripts/runVectorSearchBaseline.sh
@@ -7,16 +7,14 @@ run() {
 	residx=$1
 	resfileBase="$outdir/baseline$residx.txt"
 	resfileSearch="$outdir/search$residx.txt"
-	nofFeatures=`strusInspectVectorStorage -s "path=$vsmdir/vsm" nofFeat`
-	feat1=`shuf -i 0-$nofFeatures -n 1`
-	feat2=`shuf -i 0-$nofFeatures -n 1`
-	featname1=`strusInspectVectorStorage -s "path=$vsmdir/vsm" featName $feat1`
-	featname2=`strusInspectVectorStorage -s "path=$vsmdir/vsm" featName $feat2`
-	echo "Find $featname1 + $featname2"
-	echo "Find $featname1 + $featname2" > $resfileBase
-	echo "Find $featname1 + $featname2" > $resfileSearch
-	strusInspectVectorStorage -T 20 -N 100 -x -s "path=$vsmdir/vsm" opfeatwname \%$feat1 \+ \%$feat2 > $resfileBase
-	strusInspectVectorStorage -T 20 -N 20     -s "path=$vsmdir/vsm" opfeatwname \%$feat1 \+ \%$feat2 > $resfileSearch
+	nofFeatures=`strusInspectVectorStorage -s "path=$vsmdir/vsm" noffeat`
+	feat=`shuf -i 0-$nofFeatures -n 1`
+	featname=`strusInspectVectorStorage -s "path=$vsmdir/vsm" featname $feat`
+	echo "Find $featname"
+	echo "Find $featname" > $resfileBase
+	echo "Find $featname" > $resfileSearch
+	strusInspectVectorStorage -t 20 -N 100 -x -s "path=$vsmdir/vsm" opfeatwname \%$feat1 > $resfileBase
+	strusInspectVectorStorage -t 20 -N 20     -s "path=$vsmdir/vsm" opfeatwname \%$feat1 > $resfileSearch
 }
 
 for ii in $(seq 1 100); do echo `run $ii`; done
