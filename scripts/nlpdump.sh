b70ec452af18f12bb52a7af69d4ce411359ebbf9
patrickfrey <patrickpfrey@yahoo.com>
2017-03-01 15:08:50 +0100

    fixed bug in punctuation configuration for NLP

--- scripts/nlpdump.sh
+++ scripts/nlpdump.sh
@@ -20,7 +20,7 @@ tar -C $prefix$jobid/ -xvzf $1
 for ff in `find $prefix$jobid/ -name "*.xml" | sort`
 do
 echo "analyze $ff"
-strusAnalyze -M /usr/local/lib/strus/modules -R "$srcprefix"resources/ -D "orig,sent=' .\n',para=' .\n',start=' .\n'" "$srcprefix"config/wikipedia.ana $ff | iconv -c -f utf-8 -t utf-8 - | "$srcprefix"scripts/nlpclean.sh | perl -C -pe 's/[~_=\/\#-\+\-]/ /g' | perl -C -pe 's/[\<\>()\|\[\]\{\}]/ \,/g' | perl -C -pe 's/[,]+\./\./g' | perl -C -pe 's/([\.])(^[0-9])/ \1 \2/g' | perl -C -pe 's/([\;\,\!\?\:])/ \1 /g' | perl -C -pe 's/([\\])/ /g' >> $outputfile
+strusAnalyze -M /usr/local/lib/strus/modules -R "$srcprefix"resources/ -D "orig,sent=' .\n',para=' .\n',start=' .\n'" "$srcprefix"config/wikipedia_nlp.ana $ff | iconv -c -f utf-8 -t utf-8 - | "$srcprefix"scripts/nlpclean.sh | perl -C -pe 's/[~_=\/\#-\+\-]/ /g' | perl -C -pe 's/[\<\>()\|\[\]\{\}]/ \,/g' | perl -C -pe 's/[,]+\./\./g' | perl -C -pe 's/([\.])(^[0-9])/ \1 \2/g' | perl -C -pe 's/([\;\,\!\?\:])/ \1 /g' | perl -C -pe 's/([\\])/ /g' >> $outputfile
 done
 
 rm -Rf $prefix$jobid/
