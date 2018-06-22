8718b36dfcd81e89f1fe64775b8f8d845b085951
patrickfrey <patrickpfrey@yahoo.com>
2018-06-22 15:01:53 +0200

    example scripts -- in progress

--- examples/POST_contentstats_transaction.sh
+++ examples/POST_contentstats_transaction.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-. servername.sh.inc
-curl -i -H "Accept: text/plain" -X POST "${SERVER_ADDR}/contentstats/wikipedia/transaction"
+BASEDIR=`dirname "$0"`
+. ${BASEDIR}/servername.sh.inc
+
+TRANSACTION=`curl -i -H "Accept: text/plain" -X POST "${SERVER_ADDR}/contentstats/wikipedia/transaction"`
+curl -d "@/srv/wikipedia/raw/wikipedia0001.xml" -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "http://${SERVER_ADDR}/transaction/${TRANSACTION}"
+
+echo "LINK $TRANSACTION"
 
--- examples/PUT_contentstats.sh
+++ examples/PUT_contentstats.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-. servername.sh.inc
+BASEDIR=`dirname "$0"`
+. ${BASEDIR}/servername.sh.inc
+
 curl -d "@examples/contentstats.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "http://${SERVER_ADDR}/contentstats/wikipedia"
