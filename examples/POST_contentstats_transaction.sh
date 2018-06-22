84164d8564aa1507ad6fd76e90795abc1447995d
patrickfrey <patrickpfrey@yahoo.com>
2018-06-22 15:07:33 +0200

    examples -- in progress

--- examples/POST_contentstats_transaction.sh
+++ examples/POST_contentstats_transaction.sh
@@ -3,7 +3,7 @@
 BASEDIR=`dirname "$0"`
 . ${BASEDIR}/servername.sh.inc
 
-TRANSACTION=`curl -i -H "Accept: text/plain" -X POST "${SERVER_ADDR}/contentstats/wikipedia/transaction"`
+TRANSACTION=`curl -H "Accept: text/plain" -X POST "${SERVER_ADDR}/contentstats/wikipedia/transaction"`
 curl -d "@/srv/wikipedia/raw/wikipedia0001.xml" -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "http://${SERVER_ADDR}/transaction/${TRANSACTION}"
 
 echo "LINK $TRANSACTION"
