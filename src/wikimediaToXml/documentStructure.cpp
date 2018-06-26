002567971c553723cee8fe35ca70647e8c423436
patrickfrey <patrickpfrey@yahoo.com>
2018-06-26 11:37:22 +0200

    fixed bug in last checkin

--- src/wikimediaToXml/documentStructure.cpp
+++ src/wikimediaToXml/documentStructure.cpp
@@ -349,7 +349,7 @@ static std::string getIdentifier( const std::string& txt)
 			++si;
 		}
 	}
-	if (!rt.empty() && rt.back() == '_') rt.pop_back();
+	if (!rt.empty() && rt[ rt.size()-1] == '_') rt.pop_back();
 	return rt;
 }
 
