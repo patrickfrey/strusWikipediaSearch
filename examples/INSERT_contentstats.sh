f7d05069f0ac0c2c01281dc303afbbd904ad0345
patrickfrey <patrickpfrey@yahoo.com>
2018-08-12 14:34:42 +0200

    ...

--- examples/INSERT_contentstats.sh
+++ examples/INSERT_contentstats.sh
@@ -5,7 +5,7 @@ TRANSACTION=`curl -H "Accept: text/plain" -X POST "${SERVER_ADDR}/contentstats/w
 cd /srv/wikipedia/doc
 # for ii in 0 1 2 3 4 5 6 7 8 9; do
 for ii in 0; do
-	for xx in `ls -d "$ii*"`; do
+	for xx in `ls -d $ii*`; do
 		for dd in `find /srv/wikipedia/doc/$xx -name "*.xml"`; do
 			echo "--- Collect statistics $xx ---"
 			curl -d "@$dd" -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "$TRANSACTION"
