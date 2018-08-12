76d768750c3b6addcd21f3c90d2e3b886c98fafc
patrickfrey <patrickpfrey@yahoo.com>
2018-08-12 14:12:19 +0200

    added script to analyze content statistics

---
+++ examples/INSERT_contentstats.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "--- Analyze document contents ---"
+TRANSACTION=`curl -H "Accept: text/plain" -X POST "$SERVER/contentstats/wikipedia/transaction"
+cd /srv/wikipedia/doc
+# for ii in 0 1 2 3 4 5 6 7 8 9; do
+for ii in 0; do
+	for xx in `ls "$ii*"`; do
+		for dd in `find /srv/wikipedia/doc/$xx -name "*.xml"`; do
+			echo "--- Collect statistics $xx ---"
+			curl -d "@$dd" -i -H "Accept: text/plain" -H "Content-Type: application/xml; charset=UTF-8" -X PUT "$TRANSACTION"
+		done
+	done
+done
+echo $TRANSACTION
+
