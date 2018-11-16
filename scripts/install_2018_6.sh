506bd1e415dc01bb31a797127d40441fd723f3fb
PatrickFrey <patrickpfrey@yahoo.com>
2018-11-16 14:00:40 +0100

    temporary checkin

--- scripts/install_2018_6.sh
+++ scripts/install_2018_6.sh
@@ -16,7 +16,7 @@
 #	Python:
 #		numpy
 
-USER=patrick:patrick
+export USER=patrick:patrick
 
 sudo mkdir /srv/wikipedia
 sudo chown $USER /srv/wikipedia
@@ -27,29 +27,31 @@ wget http://dumps.wikimedia.your.org/enwiki/latest/enwiki-latest-pages-articles.
 bunzip2 enwiki-latest-pages-articles.xml.bz2
 
 mkdir -p xml
+mkdir -p nlpxml
 strusWikimediaToXml -n 0 -P 10000 -R ./redirects.txt enwiki-latest-pages-articles.xml
 strusWikimediaToXml -I -B -n 0 -P 10000 -t 12 -L ./redirects.txt enwiki-latest-pages-articles.xml xml
 
 for ext in err mis wtf org; do find xml -name "*.$ext" | xargs rm; done
 
 export PYTHONHASHSEED=123
-POSTAGGER_TAG_OPT="-D '; ' ' -X '//pagelink@id' -Y '##' -e '//pagelink()' -e '//weblink()' -e '//text()' -e '//attr()' -e '//char()' -e '//math()' -e '//code()' -e '//bibref()' -E '//entity' -E '//attr' -E '//attr~' -E '//quot' -E '//quot~' -p '//heading' -p '//table' -p '//citation' -p '//ref' -p '//list' -p '//cell~' -p '//head~' -p '//heading~' -p '//list~' -p '//br'"
+POSTAGGER_TAG_OPT="-D '; ' ' -X '//pagelink@id' -Y '##' -e '//pagelink()' -e '//weblink()' -e '//text()' -e '//attr()' -e '//char()' -e '//math()' -e '//code()' -e '//bibref()' -E '//entity' -E '//attr' -E '//attr~' -E '//quot' -E '//quot~' -E '//pagelink' -E '//weblink' -E '//tablink' -E '//citlink' -E '//reflink' -E '//tabtitle' -E '//head' -E '//cell' -p '//heading' -p '//table' -p '//citation' -p '//ref' -p '//list' -p '//cell~' -p '//head~' -p '//heading~' -p '//list~' -p '//br'"
 
 for aa in 0 1 2 3 4 5 6 7 8 9; do
 for bb in 0 1 2 3 4 5 6 7 8 9; do
 for cc in 0 1 2 3 4 5 6 7 8 9; do
 for dd in 0 1 2 3 4 5 6 7 8 9; do
 	DID=$aa$bb$cc$dd
-	strusPosTagger -I -x xml -C XML -D '; ' -X '//pagelink@id' -Y '##' -e '//pagelink()' -e '//weblink()' -e '//text()' -e '//attr()' -e '//char()' -e '//math()' -e '//code()' -e '//bibref()' -E '//entity' -E '//attr' -E '//attr~' -E '//quot' -E '//quot~' -p '//heading' -p '//table' -p '//citation' -p '//ref' -p '//list' -p '//cell~' -p '//head~' -p '//heading~' -p '//list~' -p '//br' /srv/wikipedia/xml/$DID /srv/wikipedia/pos/$DID.txt
+	echo "processing $DID ..."
+	strusPosTagger -I -x xml -C XML -D '; ' -X '//pagelink@id' -Y '##' -e '//pagelink()' -e '//weblink()' -e '//text()' -e '//attr()' -e '//char()' -e '//math()' -e '//code()' -e '//bibref()' -E '//entity' -E '//attr' -E '//attr~' -E '//quot' -E '//quot~' -E '//pagelink' -E '//weblink' -E '//tablink' -E '//citlink' -E '//reflink' -E '//tabtitle' -E '//head' -E '//cell' -p '//heading' -p '//table' -p '//citation' -p '//ref' -p '//list' -p '//cell~' -p '//head~' -p '//heading~' -p '//list~' -p '//br' /srv/wikipedia/xml/$DID /srv/wikipedia/pos/$DID.txt
 done
 done
 done
 done
 
 
-cat /srv/wikipedia/pos/0000.txt | scripts/strusnlp_tagger.py -C 1 > /srv/wikipedia/tag/0000.txt
+cat /srv/wikipedia/pos/0000.txt | scripts/strusnlp_spacy.py -S -C 10 > /srv/wikipedia/tag/0000.txt
 DID=0000
-strusPosTagger -o odir -I -x xml -C XML $POSTAGGER_TAG_OPT /srv/wikipedia/doc/$DID /srv/wikipedia/pos/$DID.txt
+strusPosTagger -x ".xml" -C XML -e '//pagelink()' -e '//weblink()' -e '//text()' -e '//attr()' -e '//char()' -e '//math()' -e '//code()' -e '//bibref()' -o /srv/wikipedia/nlpxml/$DID /srv/wikipedia/xml/$DID /srv/wikipedia/tag/$DID.txt
 
-strusPosTagger -o odir -I -x xml -C XML $POSTAGGER_TAG_OPT Analysis.xml t0000.txt
+
 
