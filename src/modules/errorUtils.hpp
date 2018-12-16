79868f2b143b0300da79231526d4eaa5eaf081c6
PatrickFrey <patrickpfrey@yahoo.com>
2018-12-16 19:45:07 +0100

    added src subdirectory modules

---
+++ src/modules/errorUtils.hpp
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2014 Patrick P. Frey
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/// \brief Macros, classes and functions supporting error handling
+/// \file errorUtils.hpp
+#ifndef _STRUS_WIKIPEDIASEARCH_ERROR_UTILITIES_HPP_INCLUDED
+#define _STRUS_WIKIPEDIASEARCH_ERROR_UTILITIES_HPP_INCLUDED
+#include <stdexcept>
+#define _TXT(txt) txt  // No internationalization of the error messages
+
+#define THIS_COMPONENT_NAME	"strus base"
+
+/// \brief strus toplevel namespace
+namespace strus
+{
+
+#define CATCH_ERROR_MAP( contextExplainText, errorBuffer)\
+	catch (const std::bad_alloc&)\
+	{\
+		(errorBuffer).report( ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+	}\
+	catch (const std::runtime_error& err)\
+	{\
+		(errorBuffer).report( ErrorCodeRuntimeError, contextExplainText, err.what());\
+	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+	}\
+	catch (const std::exception& err)\
+	{\
+		(errorBuffer).report( ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+	}
+
+#define CATCH_ERROR_ARG1_MAP( contextExplainText, ARG, errorBuffer)\
+	catch (const std::bad_alloc&)\
+	{\
+		(errorBuffer).report( ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+	}\
+	catch (const std::runtime_error& err)\
+	{\
+		(errorBuffer).report( ErrorCodeRuntimeError, contextExplainText, ARG, err.what());\
+	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+	}\
+	catch (const std::exception& err)\
+	{\
+		(errorBuffer).report( ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+	}
+
+#define CATCH_ERROR_MAP_RETURN( contextExplainText, errorBuffer, errorReturnValue)\
+	catch (const std::bad_alloc&)\
+	{\
+		(errorBuffer).report( ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+		return errorReturnValue;\
+	}\
+	catch (const std::runtime_error& err)\
+	{\
+		(errorBuffer).report( ErrorCodeRuntimeError, contextExplainText, err.what());\
+		return errorReturnValue;\
+	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		return errorReturnValue;\
+	}\
+	catch (const std::exception& err)\
+	{\
+		(errorBuffer).report( ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		return errorReturnValue;\
+	}
+
+#define CATCH_ERROR_ARG1_MAP_RETURN( contextExplainText, ARG, errorBuffer, errorReturnValue)\
+	catch (const std::bad_alloc&)\
+	{\
+		(errorBuffer).report( ErrorCodeOutOfMem, _TXT("memory allocation error in %s"), THIS_COMPONENT_NAME);\
+		return errorReturnValue;\
+	}\
+	catch (const std::runtime_error& err)\
+	{\
+		(errorBuffer).report( ErrorCodeRuntimeError, contextExplainText, ARG, err.what());\
+		return errorReturnValue;\
+	}\
+	catch (const std::logic_error& err)\
+	{\
+		(errorBuffer).report( ErrorCodeLogicError, _TXT("logic error in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		return errorReturnValue;\
+	}\
+	catch (const std::exception& err)\
+	{\
+		(errorBuffer).report( ErrorCodeUncaughtException, _TXT("uncaught exception in %s: %s"), THIS_COMPONENT_NAME, err.what());\
+		return errorReturnValue;\
+	}
+
+}//namespace
+#endif
---
+++ src/modules/libstrus_module_normalizer_entityid.cpp
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2018 Patrick P. Frey
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#include "strus/analyzerModule.hpp"
+#include "strus/base/dll_tags.hpp"
+#include "strus/normalizerFunctionInterface.hpp"
+#include "strus/normalizerFunctionInstanceInterface.hpp"
+#include "strus/errorBufferInterface.hpp"
+#include "errorUtils.hpp"
+#include <string>
+#include <cstring>
+
+using namespace strus;
+
+#define NORMALIZER_NAME "entityid"
+#define _TXT(txt) txt  // No internationalization of the error messages for this module
+
+static const char g_delimiters[] = "\"’`'?!/;:.,-— )([]{}<>_";
+static const char g_quotes[] = "\"'’`;.:";
+
+class EntityIdNormalizerInstance
+	:public NormalizerFunctionInstanceInterface
+{
+public:
+	explicit EntityIdNormalizerInstance( ErrorBufferInterface* errorhnd_)
+		:m_errorhnd(errorhnd_){}
+
+	virtual std::string normalize(
+			const char* src,
+			std::size_t srcsize) const
+	{
+		try
+		{
+			std::string rt;
+			char const* si = src;
+			char const* se = src + srcsize;
+			for (;si < se && (0!=std::strchr( g_quotes, *si) || (unsigned char)*si <= 32); ++si){}
+			for (;si < se && (0!=std::strchr( g_quotes, *(se-1)) || (unsigned char)*(se-1) <= 32); --se){}
+			const char* start = si;
+			for (; si < se; ++si)
+			{
+				if (0!=std::strchr( g_delimiters, *si))
+				{
+					rt.append( start, si-start);
+					bool hasOnlySpace = (unsigned char)*si <= 32 || *si == '_';
+					for (++si; si < se && (0!=std::strchr( g_delimiters, *si) || (unsigned char)*si <= 32); ++si)
+					{
+						hasOnlySpace &= (unsigned char)*si <= 32 || *si == '_';
+					}
+					rt.push_back( hasOnlySpace ? '_':'-');
+					start = si;
+				}
+			}
+			rt.append( start, se-start);
+			return rt;
+		}
+		CATCH_ERROR_ARG1_MAP_RETURN( _TXT("error in %s normalize: %s"), NORMALIZER_NAME, *m_errorhnd, std::string());
+	}
+
+	virtual analyzer::FunctionView view() const
+	{
+		try
+		{
+			return analyzer::FunctionView( NORMALIZER_NAME)
+			;
+		}
+		CATCH_ERROR_MAP_RETURN( _TXT("error in introspection: %s"), *m_errorhnd, analyzer::FunctionView());
+	}
+
+private:
+	ErrorBufferInterface* m_errorhnd;
+};
+
+class EntityIdNormalizerFunction
+	:public NormalizerFunctionInterface
+{
+public:
+	explicit EntityIdNormalizerFunction( ErrorBufferInterface* errorhnd_)
+		:m_errorhnd(errorhnd_){}
+
+	virtual NormalizerFunctionInstanceInterface* createInstance( const std::vector<std::string>& args, const TextProcessorInterface*) const
+	{
+		try
+		{
+			if (!args.empty())
+			{
+				m_errorhnd->report( ErrorCodeInvalidArgument, _TXT("no arguments expected for normalizer '%s'"), NORMALIZER_NAME);
+				return 0;
+			}
+			return new EntityIdNormalizerInstance( m_errorhnd);
+		}
+		CATCH_ERROR_MAP_RETURN( _TXT("error creating instance of normalizer: %s"), *m_errorhnd, 0);
+	}
+
+	virtual const char* getDescription() const
+	{
+		return _TXT("Normalizer for entities in the Wikipedia search project");
+	}
+
+private:
+	ErrorBufferInterface* m_errorhnd;
+};
+
+/// \brief Constructor function
+static NormalizerFunctionInterface* createEntityIdNormalizerFunction( ErrorBufferInterface* errorhnd)
+{
+	try
+	{
+		return new EntityIdNormalizerFunction( errorhnd);
+	}
+	CATCH_ERROR_MAP_RETURN( _TXT("error in constructor function of normalizer: %s"), *errorhnd, 0);
+}
+
+static strus::NormalizerConstructor g_normalizers[] = { {"entityid",&createEntityIdNormalizerFunction}, {0,0} };
+
+extern "C" DLL_PUBLIC strus::AnalyzerModule entryPoint;
+strus::AnalyzerModule g_moduleheader( 0, g_normalizers, 0);
