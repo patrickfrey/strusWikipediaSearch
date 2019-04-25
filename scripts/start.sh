52da21b2d71b00f3a32fa45b650bf46fa29e2b7e
PatrickFrey <patrickpfrey@yahoo.com>
2019-04-25 08:20:14 +0200

    working on configuration -- in progress (2)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-ISERVER1="http://127.0.0.1:7184"
-ISERVER2="http://127.0.0.1:7185"
-ISERVER3="http://127.0.0.1:7186"
-ISERVER4="http://127.0.0.1:7187"
-VSERVER1="http://127.0.0.1:7191"
+export ISERVER1="http://127.0.0.1:7184"
+export ISERVER2="http://127.0.0.1:7185"
+export ISERVER3="http://127.0.0.1:7186"
+export ISERVER4="http://127.0.0.1:7187"
+export VSERVER1="http://127.0.0.1:7191"
 
-strusWebService -c config.irsv1.js -V
-strusWebService -c config.irsv2.js -V
-strusWebService -c config.irsv3.js -V
-strusWebService -c config.irsv4.js -V
-strusWebService -c config.vrsv1.js -V
+strusWebService -c build/config/config_isrv1.js -V
+strusWebService -c build/config/config_isrv2.js -V
+strusWebService -c build/config/config_isrv3.js -V
+strusWebService -c build/config/config_isrv4.js -V
+strusWebService -c build/config/config_vsrv1.js -V
 
 echo "--- Create storages ---"
-curl -d "@examples/istorage1.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER1/storage/istorage"
-curl -d "@examples/istorage2.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER2/storage/istorage"
-curl -d "@examples/istorage3.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER3/storage/istorage"
-curl -d "@examples/istorage4.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$SERVER4/storage/istorage"
+curl -d "@build/config/istorage1.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$ISERVER1/storage/istorage"
+curl -d "@build/config/istorage2.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$ISERVER2/storage/istorage"
+curl -d "@build/config/istorage3.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$ISERVER3/storage/istorage"
+curl -d "@build/config/istorage4.json" -i -H "Accept: text/plain" -H "Content-Type: application/json; charset=UTF-8" -X PUT "$ISERVER4/storage/istorage"
 
